dc479bdc60fcd7426e44ba721696862413c0d173
suleabel <31124954+suleabel@users.noreply.github.com>
2020-03-07 00:56:49 +0100

    add tomcat deploy plugin

--- existDB-backend/src/test/java/hu/sule/administration/AdministratorApplicationTests.java
+++ existDB-backend/src/test/java/hu/sule/administration/AdministratorApplicationTests.java
@@ -1,12 +1,14 @@
-package com.example.demo;
+package hu.sule.administration;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = {AdministratorApplication.class})
+@AutoConfigureMockMvc
 public class AdministratorApplicationTests {
 
 	@Test
