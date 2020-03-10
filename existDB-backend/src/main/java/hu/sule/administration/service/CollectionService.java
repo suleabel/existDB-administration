a26274996670a46cc7fa6c2ff46497b1163eed42
suleabel <31124954+suleabel@users.noreply.github.com>
2020-03-10 22:19:30 +0100

    add gitignore to angular project

--- existDB-backend/src/main/java/hu/sule/administration/service/CollectionService.java
+++ existDB-backend/src/main/java/hu/sule/administration/service/CollectionService.java
@@ -4,22 +4,12 @@ import hu.sule.administration.model.ExistFileManagerModel;
 import hu.sule.administration.model.ForStoreResourceAndColl;
 import hu.sule.administration.model.ResourceReadModel;
 import hu.sule.administration.queries.ExistDbCollectionManagerQueries;
-import org.jdom2.Document;
-import org.jdom2.Element;
-import org.jdom2.JDOMException;
-import org.jdom2.input.SAXBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.xml.sax.InputSource;
-
-import javax.annotation.Resource;
-import java.io.IOException;
-import java.io.StringReader;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+
 
 @Service
 public class CollectionService {
